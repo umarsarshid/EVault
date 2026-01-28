c450bc3181fda1f5d50ba7f55c67288f205ff79d
pabloarshid <umarsa.birvancity@gmail.com>
2026-01-28 14:30:00 -0800

    fix remove unused input import

--- src/app/Landing.tsx
+++ src/app/Landing.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 import Card from '../components/Card'
-import Input from '../components/Input'
 import { useVault } from './VaultContext'
 
 export default function Landing() {
